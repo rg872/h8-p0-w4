97a40cbe2cb4489472e05d6037a376d7893c178a
Rafie Gilang <gilang0293@gmail.com>
2018-02-15 06:12:43 +0700

    complete exercises-8

---
+++ Exercises-8.js
@@ -0,0 +1,13 @@
+function urutkanAbjad(str) {
+
+  var test = str.split('');
+  test = test.sort();
+  return test;
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
